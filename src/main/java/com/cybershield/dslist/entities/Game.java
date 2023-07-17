24ba6072fb087e70990c9280c5b079ce6e6f6256
unknown <deybisonbraga@hotmail.com>
2023-07-16 23:42:21 -0300

    Game seed

--- src/main/java/com/cybershield/dslist/entities/Game.java
+++ src/main/java/com/cybershield/dslist/entities/Game.java
@@ -14,10 +14,13 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
-    private String shotDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 
@@ -25,16 +28,16 @@ public class Game {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
-                String shotDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+                String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
-        this.shotDescription = shotDescription;
+        this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
 
@@ -70,12 +73,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
@@ -94,12 +97,12 @@ public class Game {
         this.imgUrl = imgUrl;
     }
 
-    public String getShotDescription() {
-        return shotDescription;
+    public String getShortDescription() {
+        return shortDescription;
     }
 
-    public void setShotDescription(String shotDescription) {
-        this.shotDescription = shotDescription;
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
     }
 
     public String getLongDescription() {
